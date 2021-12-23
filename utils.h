547d394bb040820ec7e73a72bbb4f344593497d6
csegura <carlossegurag@gmail.com>
2021-12-22 22:13:14 -0600

    Remove old version

--- utils.h
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************************
-Authors: Carlos Segura (Programmer and Designer), Lazaro Lugo (Programmer and Designer),
-Gara Miranda (Designer)
-
-Description: functions related to generation of random numbers.
-********************************************************************************************/
-
-int getRandomInteger0_N(int n);//get random integer [0, n]
-double generateRandomDouble0_Max(double maxValue);
